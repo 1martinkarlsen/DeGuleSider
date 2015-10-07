7b267f0dc8619c1a19b8884c84f4e85fe573a83a
KSWorm <kasper@sylvestworm.dk>
2015-10-07 13:33:25 +0200

    Fixet fejlen i ModelFacade classen

--- src/java/control/ModelFacade.java
+++ src/java/control/ModelFacade.java
@@ -1,5 +1,6 @@
 package control;
 
+import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -8,48 +9,59 @@ import javax.persistence.Query;
 import model.Person;
 
 public class ModelFacade {
-    
+
     EntityManagerFactory emf;
     EntityManager em;
     Query query;
-    
+
     Person person;
-    
-    List<Person> personList;
-    
+
+    List<Person> personList = new ArrayList();
+
     public static void main(String[] args) {
         ModelFacade facade = new ModelFacade();
+
         facade.testRun();
-        
+
         facade.getPersons();
     }
-    
+
     public void testRun() {
         Person p1 = new Person();
         Person p2 = new Person();
         Person p3 = new Person();
-        
+
+        p1.setFirstName("Kasper");
+        p1.setLastName("Worm");
+
+        p2.setFirstName("Rasmus");
+        p2.setLastName("hansen");
+
+        p3.setFirstName("Martin");
+        p3.setLastName("Karlsen");
+
         personList.add(p1);
         personList.add(p2);
         personList.add(p3);
+
     }
-    
+
     public ModelFacade() {
-        if(emf == null) {
+        if (emf == null) {
             emf = Persistence.createEntityManagerFactory("DeGuleSiderPU");
         }
-        
+
         try {
             em = emf.createEntityManager();
         } catch (Exception e) {
             System.out.println(e);
         }
     }
-    
+
     public List<Person> getPersons() {
         query = em.createNamedQuery("Person.findAll");
-        
+
         return personList = query.getResultList();
     }
-    
+
 }
